feecd942d69b19246009c9250a37335f1fe6bbeb
liuyudong <lydshdx@163.com>
2022-07-30 18:40:47 +0800

    提交到hot-fix分支

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,6 +3,7 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git1!");
         System.out.println("Hello git4!");
+        System.out.println("Hello hot-fix!");
     }
 
 }
